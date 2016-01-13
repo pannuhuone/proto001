5d9230f9885899f4c406e5a66ce0b161c05d3e3b
pannuhuone <tao@iki.fi>
2016-01-14 00:43:08 +0200

    Custom links for Log In and Sign In without social logins. Some useraccounts:bootstrap configurations.

--- server/lib/config/accounts.js
+++ server/lib/config/accounts.js
@@ -27,6 +27,7 @@ Meteor.startup(function() {
   */
 
   // Add Google configuration entry
+    /*
   ServiceConfiguration.configurations.update(
     { service: "google" },
     { $set: {
@@ -37,6 +38,7 @@ Meteor.startup(function() {
     },
     { upsert: true }
   );
+  */
 
   // Add Linkedin configuration entry
   /*
